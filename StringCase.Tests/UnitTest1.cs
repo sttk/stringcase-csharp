64c3fea41951456b76c197a7dcf60c50e15b682c
Takayuki Sato <sttk.xslet@gmail.com>
2026-08-21 01:26:05 +0900

    chore: set up the development environment (#1)

---
+++ StringCase.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace StringCase.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ StringCase/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace StringCase;
+
+public class Class1
+{
+
+}
